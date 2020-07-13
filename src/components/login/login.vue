85af17f4735d5284333adf270de577caca7d57fb
yangyy <215661582@qq.com>
2020-07-13 19:01:55 +0800

    登录页面制作

---
+++ src/components/login/login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login-wrap">
+    <el-form class="login-form" :model="formdata" label-position="top" label-width="80px">
+      <h1>用户登录</h1>
+      <el-form-item label="用户名">
+        <el-input v-model="formdata.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formdata.password"></el-input>
+      </el-form-item>
+      <el-button class="login-btn" type="primary">登录</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login-wrap{
+    height: 100%;
+    background-color: #324152;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.login-form{
+    width: 400px;
+    background-color: #fff;
+    border-radius: 5px;
+    padding: 30px;
+}
+.login-btn{
+    width: 100%;
+}
+</style>
